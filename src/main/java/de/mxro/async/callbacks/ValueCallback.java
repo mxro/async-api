ba93570dcc3ee7c1a05d327d5e13cbcf0015bdb7
mxro <mxro@nowhere.com>
2014-08-07 07:37:07 +1000

    d

--- src/main/java/de/mxro/async/callbacks/ValueCallback.java
+++ src/main/java/de/mxro/async/callbacks/ValueCallback.java
@@ -1,13 +1,11 @@
 package de.mxro.async.callbacks;
 
-public interface ValueCallback<T> {
+public interface ValueCallback<T> extends FailureCallback {
 	
 	/**
 	 * Asynchronous operation successfully completed.
 	 * @param value
 	 */
 	public void onSuccess(T value);
-	
-	public void onFailure(Throwable t);
 	
 }
