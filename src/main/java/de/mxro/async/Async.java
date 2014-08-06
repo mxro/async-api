b594f38854f327715f8051b13f61305185570f0e
mxro <mxro@nowhere.com>
2014-08-07 07:52:03 +1000

    Consolidating most useful asynchronous utilities into dedicated projects Autocommit Thu Aug 07 07:52:03 EST 2014

--- src/main/java/de/mxro/async/Async.java
+++ src/main/java/de/mxro/async/Async.java
@@ -1,8 +1,17 @@
 package de.mxro.async;
 
+import java.util.List;
+
+import de.mxro.async.callbacks.ListCallback;
+import de.mxro.async.flow.CallbackMap;
+
 public class Async {
 
-	public static <T> void map(T.. input, ListCallback<T> callback) {
+	public static <InputType, ResultType> void map(List<InputType> inputs, ListCallback<ResultType> callback) {
+		
+		final CallbackMap<InputType,ResultType> callbackMap = new CallbackMap<InputType, ResultType>(inputs, callback);
+		
+		
 		
 	}
 	
--- src/main/java/de/mxro/async/callbacks/ListCallback.java
+++ src/main/java/de/mxro/async/callbacks/ListCallback.java
@@ -7,8 +7,7 @@ package de.mxro.async.callbacks;
 
 import java.util.List;
 
-public interface ListCallback<GResponse> {
-	public void onSuccess(List<GResponse> responses);
-	public void onFailure(Throwable t);
+public interface ListCallback<GResponse> extends ValueCallback<List<GResponse>> {
+	
 	
 }
