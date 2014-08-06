82d8c8144a8c47bd43d511caf46febbcd7778c4f
mxro <mxro@nowhere.com>
2014-08-07 08:21:03 +1000

    Consolidating most useful asynchronous utilities into dedicated projects Autocommit Thu Aug 07 08:21:03 EST 2014

--- src/main/java/de/mxro/async/Async.java
+++ src/main/java/de/mxro/async/Async.java
@@ -9,6 +9,15 @@ import de.mxro.fn.Closure2;
 
 public class Async {
 
+	/**
+	 * <p>Will apply the <b>asynchronous</b> operation operation to all inputs and call the callback once all operations are completed.
+	 * <p>Callback is also called upon the first operation which fails.
+	 * <p>ValueCallback must be called in closure.
+	 * 
+	 * @param inputs
+	 * @param operation
+	 * @param callback
+	 */
 	public static <InputType, ResultType> void map(List<InputType> inputs,
 			Closure2<InputType, ValueCallback<ResultType>> operation,
 			ListCallback<ResultType> callback) {
