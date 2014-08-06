2b15c7c35a0a622e16c14351ccc6d8a25cb9b783
mxro <mxro@nowhere.com>
2014-08-07 09:26:04 +1000

    Consolidating most useful asynchronous utilities into dedicated projects Autocommit Thu Aug 07 09:26:04 EST 2014

--- src/main/java/de/mxro/async/internal/PromiseImpl.java
+++ src/main/java/de/mxro/async/internal/PromiseImpl.java
@@ -1,5 +1,6 @@
 package de.mxro.async.internal;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import de.mxro.async.AsyncPromise;
@@ -15,44 +16,48 @@ public class PromiseImpl<ResultType> implements Promise<ResultType> {
 	private ResultType resultCache;
 	private Boolean isRequesting;
 	private Throwable failureCache;
-	
+
 	private final void requestResult(final ValueCallback<ResultType> callback) {
-		
+
 		final boolean triggerOnFailure;
+		final boolean triggerOnSuccess;
 		synchronized (failureCache) {
 			triggerOnFailure = failureCache != null;
-		}
-		if (triggerOnFailure) {
-			callback.onFailure(failureCache);
-			return;
-		}
-		
-		final boolean triggerOnSuccess;
-		synchronized (resultCache) {
-			triggerOnSuccess = resultCache != null;
 
-			if (!triggerOnSuccess) {
-				synchronized (isRequesting) {
+			if (!triggerOnFailure) {
+				
+				synchronized (resultCache) {
+					triggerOnSuccess = resultCache != null;
 
-					if (isRequesting) {
-						synchronized (deferredCalls) {
-						   deferredCalls.add(callback);
+					if (!triggerOnSuccess) {
+						synchronized (isRequesting) {
+
+							if (isRequesting) {
+								synchronized (deferredCalls) {
+									deferredCalls.add(callback);
+								}
+								return;
+							} else {
+								isRequesting = true;
+							}
+
 						}
-						return;
-					} else {
-						isRequesting = true;
 					}
 
 				}
 			}
-
 		}
-		
+
+		if (triggerOnFailure) {
+			callback.onFailure(failureCache);
+			return;
+		}
+
 		if (triggerOnSuccess) {
 			callback.onSuccess(resultCache);
 			return;
 		}
-		
+
 		asyncPromise.get(new ValueCallback<ResultType>() {
 
 			@Override
@@ -60,17 +65,22 @@ public class PromiseImpl<ResultType> implements Promise<ResultType> {
 				synchronized (failureCache) {
 					failureCache = t;
 				}
-				
+
 				synchronized (deferredCalls) {
-					
+					cachedCalls = new ArrayList<ValueCallback<ResultType>>(
+							deferredCalls);
 				}
-				
+
+				for (ValueCallback<ResultType> deferredCb : deferredCalls) {
+					deferredCb.onSuccess(resultCache);
+				}
+
 				callback.onFailure(t);
 			}
 
 			@Override
 			public void onSuccess(ResultType value) {
-				
+
 			}
 		});
 
