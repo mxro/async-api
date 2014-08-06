9e79fd43a198c71e6c79863058540f9913e2093f
mxro <mxro@nowhere.com>
2014-08-07 08:56:03 +1000

    Consolidating most useful asynchronous utilities into dedicated projects Autocommit Thu Aug 07 08:56:03 EST 2014

--- src/main/java/de/mxro/async/Operation.java
+++ src/main/java/de/mxro/async/Operation.java
@@ -3,6 +3,14 @@ package de.mxro.async;
 import de.mxro.async.callbacks.ValueCallback;
 import de.mxro.fn.Closure2;
 
+/**
+ * A base template for asynchronous operations.
+ * 
+ * @author Max Rohde
+ *
+ * @param <InputType>
+ * @param <OutputType>
+ */
 public interface Operation<InputType, OutputType> extends Closure2<InputType, ValueCallback<OutputType>> {
 
 	public void apply(InputType input, ValueCallback<OutputType> callback);
