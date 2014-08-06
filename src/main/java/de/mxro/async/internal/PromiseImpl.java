6c067361f2c305f0ccb80f7074d8d1081f9a4b5a
mxro <mxro@nowhere.com>
2014-08-07 09:18:04 +1000

    Consolidating most useful asynchronous utilities into dedicated projects Autocommit Thu Aug 07 09:18:04 EST 2014

--- src/main/java/de/mxro/async/internal/PromiseImpl.java
+++ src/main/java/de/mxro/async/internal/PromiseImpl.java
@@ -13,17 +13,34 @@ public class PromiseImpl<ResultType> implements Promise<ResultType> {
 	private final List<ValueCallback<ResultType>> deferredCalls;
 
 	private ResultType resultCache;
+	private Boolean isRequesting;
 
 	private final void requestResult(ValueCallback<ResultType> callback) {
 
 		final boolean triggerOnSuccess;
 		synchronized (resultCache) {
 			triggerOnSuccess = resultCache != null;
+
+			if (!triggerOnSuccess) {
+				synchronized (isRequesting) {
+
+					if (isRequesting) {
+						deferredCalls.add(callback);
+						return;
+					} else {
+						isRequesting = true;
+					}
+
+				}
+			}
+
 		}
+		
 		if (triggerOnSuccess) {
 			callback.onSuccess(resultCache);
 			return;
 		}
+		
 		
 
 	}
