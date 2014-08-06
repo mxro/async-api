e663169b40045c1bb4a5125565f7b0ab8a8998e8
mxro <mxro@nowhere.com>
2014-08-07 07:58:55 +1000

    i

---
+++ src/main/java/de/mxro/async/callbacks/SuccessFailCallback.java
@@ -0,0 +1,11 @@
+/*******************************************************************************
+ * Copyright 2011 Max Erik Rohde http://www.mxro.de
+ * 
+ * All rights reserved.
+ ******************************************************************************/
+package de.mxro.async.callbacks;
+
+public interface SuccessFailCallback<GError> {
+	public void onSuccess();
+	public void onFail(GError message);
+}
