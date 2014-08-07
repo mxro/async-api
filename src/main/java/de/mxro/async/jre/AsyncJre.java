3a2740665e23d767e9c03e0cfe77fb6dd77d0498
mxro <mxro@nowhere.com>
2014-08-07 10:23:04 +1000

    Consolidating most useful asynchronous utilities into dedicated projects Autocommit Thu Aug 07 10:23:04 EST 2014

--- src/main/java/de/mxro/async/jre/AsyncJre.java
+++ src/main/java/de/mxro/async/jre/AsyncJre.java
@@ -2,8 +2,12 @@ package de.mxro.async.jre;
 
 import java.util.List;
 
+import de.mxro.async.Async;
 import de.mxro.async.AsyncPromise;
+import de.mxro.async.Operation;
 import de.mxro.async.Promise;
+import de.mxro.async.callbacks.ListCallback;
+import de.mxro.async.callbacks.ValueCallback;
 import de.mxro.async.jre.internal.JrePromiseImpl;
 
 public class AsyncJre {
@@ -13,10 +17,34 @@ public class AsyncJre {
 		return new JrePromiseImpl<ResultType>(promise);
 	}
 	
-	public static List<Object> all(Promise... promises) {
+	public static List<Object> parallel(Promise... promises) {
 		
+		Async.map(promises, new Operation<Promise, Object>() {
+
+			@Override
+			public void apply(Promise input,
+					ValueCallback<Object> callback) {
+				// TODO Auto-generated method stub
+				
+			}
+		}, new ListCallback<Object>() {
+
+			@Override
+			public void onSuccess(List<Object> value) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onFailure(Throwable t) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		
-		
+		for (Promise p: promises) {
+			
+		}
 	}
 	
 }
