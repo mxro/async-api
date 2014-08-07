7bf5826075748d4a5d277650a87183709cc9c98a
mxro <mxro@nowhere.com>
2014-08-07 10:18:04 +1000

    Consolidating most useful asynchronous utilities into dedicated projects Autocommit Thu Aug 07 10:18:04 EST 2014

--- src/main/java/de/mxro/async/jre/AsyncJre.java
+++ src/main/java/de/mxro/async/jre/AsyncJre.java
@@ -1,5 +1,15 @@
 package de.mxro.async.jre;
 
-public class AsyncJre {
+import de.mxro.async.AsyncPromise;
+import de.mxro.async.Promise;
+import de.mxro.async.jre.internal.JrePromiseImpl;
 
+public class AsyncJre {
+	
+	
+	public static <ResultType> Promise<ResultType> promise(
+			AsyncPromise<ResultType> promise) {
+		return new JrePromiseImpl<ResultType>(promise);
+	}
+	
 }
--- src/main/java/de/mxro/async/jre/internal/JrePromiseImpl.java
+++ src/main/java/de/mxro/async/jre/internal/JrePromiseImpl.java
@@ -1,4 +1,4 @@
-package de.mxro.async.jre;
+package de.mxro.async.jre.internal;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
