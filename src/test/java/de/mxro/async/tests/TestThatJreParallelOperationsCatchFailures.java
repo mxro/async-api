e28dccad2cbd02bdfad22fa57869d31e1c743664
mxro <mxro@nowhere.com>
2014-08-07 10:48:04 +1000

    Consolidating most useful asynchronous utilities into dedicated projects Autocommit Thu Aug 07 10:48:04 EST 2014

--- src/test/java/de/mxro/async/tests/TestThatJreParallelOperationsCatchFailures.java
+++ src/test/java/de/mxro/async/tests/TestThatJreParallelOperationsCatchFailures.java
@@ -1,12 +1,56 @@
 package de.mxro.async.tests;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import org.junit.Test;
+
+import de.mxro.async.AsyncPromise;
+import de.mxro.async.Promise;
+import de.mxro.async.callbacks.ValueCallback;
+import de.mxro.async.jre.AsyncJre;
+
 
 public class TestThatJreParallelOperationsCatchFailures {
 
+	private final class RandomlyFailingPromise implements AsyncPromise<String> {
+
+		@Override
+		public void get(final ValueCallback<String> callback) {
+			
+			new Thread() {
+
+				@Override
+				public void run() {
+					
+					if (new Random().nextInt(2) == 1) {
+						callback.onSuccess("Got it.");
+						return;
+					}
+					
+					callback.onFailure(new Exception("So failed!"));
+					
+				}
+				
+				
+				
+			}.start();
+			
+		}
+		
+		
+		
+	}
+	
 	@Test
 	public void test_it() {
-		
+		List<Promise<String>> promises = new ArrayList<Promise<String>>();
+
+		for (int i = 1; i <= 50; i++) {
+			Promise<String> p = AsyncJre.promise(new RandomlyFailingPromise());
+			promises.add(p);
+		}
 	}
 	
 }
