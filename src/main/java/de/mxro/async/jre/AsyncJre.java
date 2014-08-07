192b2cfddb13fa3bc10a2ddb2dd48de2bb54d2ac
mxro <mxro@nowhere.com>
2014-08-07 10:27:04 +1000

    Consolidating most useful asynchronous utilities into dedicated projects Autocommit Thu Aug 07 10:27:04 EST 2014

--- src/main/java/de/mxro/async/jre/AsyncJre.java
+++ src/main/java/de/mxro/async/jre/AsyncJre.java
@@ -1,5 +1,6 @@
 package de.mxro.async.jre;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
@@ -20,12 +21,14 @@ public class AsyncJre {
 		return new JrePromiseImpl<ResultType>(promise);
 	}
 	
+	@SuppressWarnings("rawtypes")
 	public static List<Object> parallel(Promise... promises) {
 		
 		final CountDownLatch latch = new CountDownLatch(1);
 		
 		Async.map(Arrays.asList(promises), new Operation<Promise, Object>() {
 
+			@SuppressWarnings("unchecked")
 			@Override
 			public void apply(Promise input,
 					final ValueCallback<Object> callback) {
@@ -66,6 +69,13 @@ public class AsyncJre {
 		}
 		
 		
+		List<Object> res = new ArrayList<Object>(promises.length);
+		
+		for (Promise p: promises) {
+			res.add(p.get());
+		}
+		
+		return res;
 		
 		
 	}
