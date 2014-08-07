2fc1bf8c9f649f414c43ba906b38fcc8bfe60d6e
mxro <mxro@nowhere.com>
2014-08-07 10:32:04 +1000

    Consolidating most useful asynchronous utilities into dedicated projects Autocommit Thu Aug 07 10:32:04 EST 2014

--- src/test/java/de/mxro/async/tests/TestThatJreParallelOperationsWork.java
+++ src/test/java/de/mxro/async/tests/TestThatJreParallelOperationsWork.java
@@ -1,5 +1,7 @@
 package de.mxro.async.tests;
 
+import java.util.Random;
+
 import org.junit.Test;
 
 import de.mxro.async.AsyncPromise;
@@ -8,26 +10,35 @@ import de.mxro.async.jre.AsyncJre;
 
 public class TestThatJreParallelOperationsWork {
 
+	private final class AsyncPromiseImplementation implements
+			AsyncPromise<String> {
+		@Override
+		public void get(final ValueCallback<String> callback) {
+			new Thread() {
+
+				@Override
+				public void run() {
+					int delay = new Random().nextInt(10)+1;
+					try {
+						Thread.sleep(delay);
+					} catch (InterruptedException e) {
+						throw new RuntimeException(e);
+					}
+					
+					callback.onSuccess("Completed after delay: "+delay);
+				}
+				
+				
+				
+			}.start();
+		}
+	}
+
 	@Test
 	public void test_it() {
 		
 		
-		AsyncJre.promise(new AsyncPromise<String>() {
-
-			@Override
-			public void get(ValueCallback<String> callback) {
-				new Thread() {
-
-					@Override
-					public void run() {
-						Thread.sleep(new Random());
-					}
-					
-					
-					
-				}.start();
-			}
-		});
+		AsyncJre.promise(new AsyncPromiseImplementation());
 		
 	}
 	
