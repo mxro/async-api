5399835724fe49dc90774a782c498467ae251b3a
mxro <mxro@nowhere.com>
2014-08-07 11:49:17 +1000

    Consolidating most useful asynchronous utilities into dedicated projects Autocommit Thu Aug 07 11:49:17 EST 2014

--- src/main/java/de/mxro/async/PartialPromise.java
+++ src/main/java/de/mxro/async/PartialPromise.java
@@ -2,6 +2,12 @@ package de.mxro.async;
 
 import de.mxro.async.callbacks.ValueCallback;
 
+/**
+ * A potential for a promise. Use Async or AsyncJre utilities to convert into full promises.
+ * @author adminuser
+ *
+ * @param <ResultType>
+ */
 public interface PartialPromise<ResultType> {
 
 	public void get(ValueCallback<ResultType> callback);
