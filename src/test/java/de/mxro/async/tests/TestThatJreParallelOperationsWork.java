ab620869ea0e776efe8d1ea17c72177dacc0133c
mxro <mxro@nowhere.com>
2014-08-07 10:31:04 +1000

    Consolidating most useful asynchronous utilities into dedicated projects Autocommit Thu Aug 07 10:31:04 EST 2014

--- src/test/java/de/mxro/async/tests/TestThatJreParallelOperationsWork.java
+++ src/test/java/de/mxro/async/tests/TestThatJreParallelOperationsWork.java
@@ -1,9 +1,33 @@
 package de.mxro.async.tests;
 
+import org.junit.Test;
+
+import de.mxro.async.AsyncPromise;
+import de.mxro.async.callbacks.ValueCallback;
+import de.mxro.async.jre.AsyncJre;
+
 public class TestThatJreParallelOperationsWork {
 
 	@Test
 	public void test_it() {
+		
+		
+		AsyncJre.promise(new AsyncPromise<String>() {
+
+			@Override
+			public void get(ValueCallback<String> callback) {
+				new Thread() {
+
+					@Override
+					public void run() {
+						Thread.sleep(new Random());
+					}
+					
+					
+					
+				}.start();
+			}
+		});
 		
 	}
 	
