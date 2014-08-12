a6eb45642b1c45349c1970e3a00b1d6a1140eb97
mxro <mxro@nowhere.com>
2014-08-12 11:19:37 +1000

    Simplifiying Service API by using reusable callbacks Autocommit Tue Aug 12 11:19:37 EST 2014

--- src/main/java/de/mxro/async/callbacks/SimpleCallback.java
+++ src/main/java/de/mxro/async/callbacks/SimpleCallback.java
@@ -5,8 +5,19 @@
  ******************************************************************************/
 package de.mxro.async.callbacks;
 
+/**
+ * <p>A very basic callback which allows for the caller to be notified of the completion of the operation.
+ * <p>This callback extends {@link FailureCallback} thus failures can be reported through the method {@link #onFailure(Throwable)}.
+ * <p>For asynchronous operations that return a value, use {@link ValueCallback} instead.
+ *  
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ *
+ */
 public interface SimpleCallback extends FailureCallback {
 	
+	/**
+	 * Called when the asynchronous operation has completed successfully.
+	 */
 	public void onSuccess();
 
 }
