f04f854b2d35c7af43b56f3903ae8e4d2c30ccdb
mxro <mxro@nowhere.com>
2014-09-15 07:57:30 +1000

    Trying to fix double obfustication bug. -> Simplifying classes causing problems Autocommit Mon Sep 15 07:57:30 EST 2014

--- src/main/java/de/mxro/async/callbacks/SimpleCallback.java
+++ src/main/java/de/mxro/async/callbacks/SimpleCallback.java
@@ -1,23 +1,24 @@
-/*******************************************************************************
- * Copyright 2011 Max Erik Rohde http://www.mxro.de
- * 
- * All rights reserved.
- ******************************************************************************/
 package de.mxro.async.callbacks;
 
 /**
- * <p>A very basic callback which allows for the caller to be notified of the completion of the operation.
- * <p>This callback extends {@link FailureCallback} thus failures can be reported through the method {@link #onFailure(Throwable)}.
- * <p>For asynchronous operations that return a value, use {@link ValueCallback} instead.
- *  
+ * <p>
+ * A very basic callback which allows for the caller to be notified of the
+ * completion of the operation.
+ * <p>
+ * This callback extends {@link FailureCallback} thus failures can be reported
+ * through the method {@link #onFailure(Throwable)}.
+ * <p>
+ * For asynchronous operations that return a value, use {@link ValueCallback}
+ * instead.
+ * 
  * @author <a href="http://www.mxro.de">Max Rohde</a>
  *
  */
 public interface SimpleCallback extends FailureCallback {
-	
-	/**
-	 * Called when the asynchronous operation has completed successfully.
-	 */
-	public void onSuccess();
+
+    /**
+     * Called when the asynchronous operation has completed successfully.
+     */
+    public void onSuccess();
 
 }
