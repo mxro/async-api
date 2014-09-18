3b88a02d42a97b3c8d9e8a8999af60b7aad79664
mxro <mxro@nowhere.com>
2014-09-18 10:23:36 +1000

    Reworking embedded server API Autocommit Thu Sep 18 10:23:36 EST 2014

--- src/main/java/de/mxro/async/Async.java
+++ src/main/java/de/mxro/async/Async.java
@@ -9,67 +9,68 @@ import de.mxro.async.flow.CallbackMap;
 import de.mxro.async.internal.PromiseImpl;
 import de.mxro.fn.Success;
 
-public class Async {
+public final class Async {
 
-	/**
-	 * <p>
-	 * Will apply the <b>asynchronous</b> operation operation to all inputs and
-	 * call the callback once all operations are completed.
-	 * <p>
-	 * Callback is also called upon the first operation which fails.
-	 * <p>
-	 * ValueCallback must be called in closure.
-	 * 
-	 * @param inputs
-	 * @param operation
-	 * @param callback
-	 */
-	public static <InputType, ResultType> void map(List<InputType> inputs,
-			Operation<InputType, ResultType> operation,
-			ListCallback<ResultType> callback) {
+    public static final <ResultType> ResultType get(final Deferred<ResultType> deferred) {
 
-		final CallbackMap<InputType, ResultType> callbackMap = new CallbackMap<InputType, ResultType>(
-				inputs, callback);
+    }
 
-		for (InputType input : inputs) {
-			operation.apply(input, callbackMap.createCallback(input));
-		}
+    /**
+     * <p>
+     * Will apply the <b>asynchronous</b> operation operation to all inputs and
+     * call the callback once all operations are completed.
+     * <p>
+     * Callback is also called upon the first operation which fails.
+     * <p>
+     * ValueCallback must be called in closure.
+     * 
+     * @param inputs
+     * @param operation
+     * @param callback
+     */
+    public static <InputType, ResultType> void map(final List<InputType> inputs,
+            final Operation<InputType, ResultType> operation, final ListCallback<ResultType> callback) {
 
-	}
+        final CallbackMap<InputType, ResultType> callbackMap = new CallbackMap<InputType, ResultType>(inputs, callback);
 
-	public final static <ResultType> Promise<ResultType> promise(
-			Deferred<ResultType> promise) {
-		return new PromiseImpl<ResultType>(promise);
-	}
+        for (final InputType input : inputs) {
+            operation.apply(input, callbackMap.createCallback(input));
+        }
 
-	public final static SimpleCallback wrap(final ValueCallback<Success> callback) {
-		return new SimpleCallback() {
-			
-			@Override
-			public void onFailure(Throwable t) {
-				callback.onFailure(t);
-			}
-			
-			@Override
-			public void onSuccess() {
-				callback.onSuccess(Success.INSTANCE);
-			}
-		};
-	}
-	
-	public final static SimpleCallback doNothing() {
-		return new SimpleCallback() {
-			
-			@Override
-			public void onFailure(Throwable t) {
-				throw new RuntimeException(t);
-			}
-			
-			@Override
-			public void onSuccess() {
-				
-			}
-		};
-	}
+    }
+
+    public final static <ResultType> Promise<ResultType> promise(final Deferred<ResultType> promise) {
+        return new PromiseImpl<ResultType>(promise);
+    }
+
+    public final static SimpleCallback wrap(final ValueCallback<Success> callback) {
+        return new SimpleCallback() {
+
+            @Override
+            public void onFailure(final Throwable t) {
+                callback.onFailure(t);
+            }
+
+            @Override
+            public void onSuccess() {
+                callback.onSuccess(Success.INSTANCE);
+            }
+        };
+    }
+
+    public final static SimpleCallback doNothing() {
+        return new SimpleCallback() {
+
+            @Override
+            public void onFailure(final Throwable t) {
+                throw new RuntimeException(t);
+            }
+
+            @Override
+            public void onSuccess() {
+
+            }
+        };
+    }
 
 }
