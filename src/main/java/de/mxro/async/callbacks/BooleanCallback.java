f25b6fbf122c100631489f0eabc8c7beaf280efe
mxro <mxro@nowhere.com>
2014-09-29 14:16:22 +1000

    add javadoc

--- src/main/java/de/mxro/async/callbacks/BooleanCallback.java
+++ src/main/java/de/mxro/async/callbacks/BooleanCallback.java
@@ -1,5 +1,12 @@
 package de.mxro.async.callbacks;
 
+/**
+ * <p>
+ * A callback for asynchronous operations which return a boolean value.
+ * 
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ *
+ */
 public interface BooleanCallback extends FailureCallback {
 
     /**
