51529289b07ef2e20af469216f38e6d4876ad0c4
mxro <mxro@nowhere.com>
2014-10-15 09:48:37 +1100

    Reorganizing Netty and http-server projects. Assembling http-server implementation using Netty 3 Autocommit Wed Oct 15 09:48:37 EST 2014

--- src/main/java/de/mxro/async/internal/PromiseImpl.java
+++ src/main/java/de/mxro/async/internal/PromiseImpl.java
@@ -12,178 +12,174 @@ import de.mxro.fn.Closure;
 
 public class PromiseImpl<ResultType> implements Promise<ResultType> {
 
-	private final Deferred<ResultType> asyncPromise;
+    private final Deferred<ResultType> asyncPromise;
 
-	private final List<ValueCallback<ResultType>> deferredCalls;
+    private final List<ValueCallback<ResultType>> deferredCalls;
 
-	private Value<ResultType> resultCache;
-	private Boolean isRequesting;
-	private Value<Throwable> failureCache;
-	private List<Closure<Throwable>> exceptionCatchers;
+    private final Value<ResultType> resultCache;
+    private Boolean isRequesting;
+    private final Value<Throwable> failureCache;
+    private final List<Closure<Throwable>> exceptionCatchers;
 
-	
-	
-	@Override
-	public void get(ValueCallback<ResultType> callback) {
-		requestResult(callback);
-	}
+    @Override
+    public void get(final ValueCallback<ResultType> callback) {
+        requestResult(callback);
+    }
 
-	private final void requestResult(final ValueCallback<ResultType> callback) {
+    private final void requestResult(final ValueCallback<ResultType> callback) {
 
-		final boolean triggerOnFailure;
-		final boolean triggerOnSuccess;
-		synchronized (failureCache) {
-			triggerOnFailure = failureCache.get() != null;
+        final boolean triggerOnFailure;
+        final boolean triggerOnSuccess;
+        synchronized (failureCache) {
+            triggerOnFailure = failureCache.get() != null;
 
-			if (!triggerOnFailure) {
+            if (!triggerOnFailure) {
 
-				synchronized (resultCache) {
-					triggerOnSuccess = resultCache.get() != null;
+                synchronized (resultCache) {
+                    triggerOnSuccess = resultCache.get() != null;
 
-					if (!triggerOnSuccess) {
-						synchronized (isRequesting) {
+                    if (!triggerOnSuccess) {
+                        synchronized (isRequesting) {
 
-							if (isRequesting) {
-								synchronized (deferredCalls) {
-									deferredCalls.add(callback);
-								}
-								return;
-							} else {
-								isRequesting = true;
-							}
+                            if (isRequesting) {
+                                synchronized (deferredCalls) {
+                                    deferredCalls.add(callback);
+                                }
+                                return;
+                            } else {
+                                isRequesting = true;
+                            }
 
-						}
-					}
+                        }
+                    }
 
-				}
-			} else {
-				triggerOnSuccess = false;
-			}
-		}
+                }
+            } else {
+                triggerOnSuccess = false;
+            }
+        }
 
-		if (triggerOnFailure) {
-			callback.onFailure(failureCache.get());
-			return;
-		}
+        if (triggerOnFailure) {
+            callback.onFailure(failureCache.get());
+            return;
+        }
 
-		if (triggerOnSuccess) {
-			callback.onSuccess(resultCache.get());
-			return;
-		}
+        if (triggerOnSuccess) {
+            callback.onSuccess(resultCache.get());
+            return;
+        }
 
-		asyncPromise.get(new ValueCallback<ResultType>() {
+        asyncPromise.get(new ValueCallback<ResultType>() {
 
-			@Override
-			public void onFailure(Throwable t) {
-				final List<ValueCallback<ResultType>> cachedCalls;
-				synchronized (failureCache) {
-					failureCache.set(t);
+            @Override
+            public void onFailure(final Throwable t) {
+                final List<ValueCallback<ResultType>> cachedCalls;
+                synchronized (failureCache) {
+                    failureCache.set(t);
 
-					synchronized (deferredCalls) {
-						cachedCalls = new ArrayList<ValueCallback<ResultType>>(
-								deferredCalls);
-					}
-				}
+                    synchronized (deferredCalls) {
+                        cachedCalls = new ArrayList<ValueCallback<ResultType>>(deferredCalls);
+                    }
+                }
 
-				for (ValueCallback<ResultType> deferredCb : cachedCalls) {
-					deferredCb.onFailure(t);
-				}
+                for (final ValueCallback<ResultType> deferredCb : cachedCalls) {
+                    deferredCb.onFailure(t);
+                }
 
-				callback.onFailure(t);
-			}
+                callback.onFailure(t);
+            }
 
-			@Override
-			public void onSuccess(ResultType value) {
-				final List<ValueCallback<ResultType>> cachedCalls;
-				synchronized (failureCache) {
+            @Override
+            public void onSuccess(final ResultType value) {
+                final List<ValueCallback<ResultType>> cachedCalls;
+                synchronized (failureCache) {
 
-					assert failureCache.get() == null;
-					synchronized (resultCache) {
-						resultCache.set(value);
+                    assert failureCache.get() == null;
+                    synchronized (resultCache) {
+                        resultCache.set(value);
 
-						synchronized (deferredCalls) {
-							cachedCalls = new ArrayList<ValueCallback<ResultType>>(
-									deferredCalls);
-						}
-					}
+                        synchronized (deferredCalls) {
+                            cachedCalls = new ArrayList<ValueCallback<ResultType>>(deferredCalls);
+                        }
+                    }
 
-				}
+                }
 
-				for (ValueCallback<ResultType> deferredCb : cachedCalls) {
-					deferredCb.onSuccess(value);
-				}
+                for (final ValueCallback<ResultType> deferredCb : cachedCalls) {
+                    deferredCb.onSuccess(value);
+                }
 
-				callback.onSuccess(value);
-			}
-		});
+                callback.onSuccess(value);
+            }
+        });
 
-	}
+    }
 
-	@Override
-	public ResultType get() {
-		
-		get(new Closure<ResultType>() {
+    @Override
+    public ResultType get() {
 
-			@Override
-			public void apply(ResultType o) {
+        get(new Closure<ResultType>() {
 
-			}
-		});
-		
-		synchronized (this.failureCache) {
-			if (this.failureCache.get() != null) {
-				throw new RuntimeException(this.failureCache.get());
-			}
-		}
+            @Override
+            public void apply(final ResultType o) {
 
-	    ResultType resultCache2;
-		synchronized (this.resultCache) {
-			resultCache2 = this.resultCache.get();
-			
-		}
-		return resultCache2;
-	}
+            }
+        });
 
-	@Override
-	public void catchExceptions(Closure<Throwable> closure) {
-		assert this.resultCache.get() == null && this.failureCache.get() == null;
-		
-		synchronized (exceptionCatchers) {
-			exceptionCatchers.add(closure);
-		}
-	}
+        synchronized (this.failureCache) {
+            if (this.failureCache.get() != null) {
+                throw new RuntimeException(this.failureCache.get());
+            }
+        }
 
-	@Override
-	public void get(final Closure<ResultType> closure) {
-		requestResult(new ValueCallback<ResultType>() {
+        ResultType resultCache2;
+        synchronized (this.resultCache) {
+            resultCache2 = this.resultCache.get();
 
-			@Override
-			public void onFailure(Throwable t) {
-				final ArrayList<Closure<Throwable>> catchers;
-				synchronized (exceptionCatchers) {
-					catchers = new ArrayList<Closure<Throwable>>(exceptionCatchers);
-				}
-				for (Closure<Throwable> exceptionCatcher: catchers) {
-					exceptionCatcher.apply(t);
-				}
-			}
+        }
+        return resultCache2;
+    }
 
-			@Override
-			public void onSuccess(ResultType value) {
-				closure.apply(value);
-			}
-		});
-	}
+    @Override
+    public void catchExceptions(final Closure<Throwable> closure) {
+        assert this.resultCache.get() == null && this.failureCache.get() == null;
 
-	public PromiseImpl(Deferred<ResultType> asyncPromise) {
-		super();
-		this.asyncPromise = asyncPromise;
-		this.deferredCalls = new LinkedList<ValueCallback<ResultType>>();
-		this.resultCache = new Value<ResultType>(null);
-		this.failureCache = new Value<Throwable>(null);
-		this.exceptionCatchers = new LinkedList<Closure<Throwable>>();
-		this.isRequesting = false;
+        synchronized (exceptionCatchers) {
+            exceptionCatchers.add(closure);
+        }
+    }
 
-	}
+    @Override
+    public void get(final Closure<ResultType> closure) {
+        requestResult(new ValueCallback<ResultType>() {
+
+            @Override
+            public void onFailure(final Throwable t) {
+                final ArrayList<Closure<Throwable>> catchers;
+                synchronized (exceptionCatchers) {
+                    catchers = new ArrayList<Closure<Throwable>>(exceptionCatchers);
+                }
+                for (final Closure<Throwable> exceptionCatcher : catchers) {
+                    exceptionCatcher.apply(t);
+                }
+            }
+
+            @Override
+            public void onSuccess(final ResultType value) {
+                closure.apply(value);
+            }
+        });
+    }
+
+    public PromiseImpl(final Deferred<ResultType> asyncPromise) {
+        super();
+        this.asyncPromise = asyncPromise;
+        this.deferredCalls = new LinkedList<ValueCallback<ResultType>>();
+        this.resultCache = new Value<ResultType>(null);
+        this.failureCache = new Value<Throwable>(null);
+        this.exceptionCatchers = new LinkedList<Closure<Throwable>>();
+        this.isRequesting = false;
+
+    }
 
 }
