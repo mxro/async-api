299adff3d3f02684b8f3f5303d8800a022276dda
mxro <mxro@nowhere.com>
2014-11-14 09:45:19 +1100

    Developing first implementation for FileSync Autocommit Fri Nov 14 09:45:19 EST 2014

--- src/main/java/de/mxro/async/flow/CallbackAggregator.java
+++ src/main/java/de/mxro/async/flow/CallbackAggregator.java
@@ -10,14 +10,37 @@ public final class CallbackAggregator<V> {
 
     final ValueCallback<List<V>> callback;
 
-    public final ValueCallback<V> createCallback() {
+    boolean exceptionReceived;
+    volatile Throwable exception;
 
+    public final ValueCallback<V> createCallback() {
+        return new ValueCallback<V>() {
+
+            @Override
+            public void onFailure(final Throwable t) {
+                synchronized (exceptionReceived) {
+                    if (exceptionReceived) {
+                        throw new RuntimeException("Exception already received.", t);
+                    }
+
+                    exceptionReceived = true;
+                }
+            }
+
+            @Override
+            public void onSuccess(final V value) {
+
+            }
+        };
     }
 
     public CallbackAggregator(final int expected, final ValueCallback<List<V>> callback) {
         super();
         this.expected = expected;
         this.callback = callback;
+
+        this.exceptionReceived = false;
+        this.exception = null;
     }
 
 }
